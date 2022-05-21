eec8813e317f85bd8ccd1be70ab59e54fb347c44
tounkara22 <tounkara@stanford.edu>
2022-05-21 15:06:35 -0400

    All added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
-/** @type {import('next').NextConfig} */
+const { i18n } = require("./next-i18next.config");
+
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  nextConfig,
+  i18n,
+  images: {
+    domains: ["mycroft.ai", "lafia-inc.github.io", "miro.medium.com"],
+  },
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { appWithTranslation } from "next-i18next";
+import Layout from "../src/components/layout/layout";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
 
-export default MyApp
+export const getStaticProps = async ({ locale }: { locale: string }) => ({
+  props: {
+    ...(await serverSideTranslations(locale, ["home_page"])),
+  },
+});
+
+export default appWithTranslation(MyApp);
